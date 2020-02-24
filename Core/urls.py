6b6b7aa76dda1fccb122a1ac592257677ca5fe29
牛恒 <hylinux@126.com>
2020-02-24 14:59:36 +0800

    create new project

--- Core/urls.py
+++ Core/urls.py
@@ -1,4 +1,4 @@
-"""back URL Configuration
+"""Core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
+    url(r'^admin/', admin.site.urls),
 ]
