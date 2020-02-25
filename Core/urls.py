0cc30a89d0ef1d150fdba3d31e83ace90760969e
liupeijiang <778376242@qq.com>
2020-02-25 12:34:10 +0800

    提交测试

--- Core/urls.py
+++ Core/urls.py
@@ -20,4 +20,5 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
 ]
